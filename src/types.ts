6a3136279c38b4768b04c9a99300a9f72b2be08f
AndreaOpazo <andree.2593@gmail.com>
2021-09-15 03:17:28 -0300

    agrego tipo product y tipo cart

--- src/types.ts
+++ src/types.ts
@@ -1,4 +1,21 @@
 export type MiddlewareParams = {
   path: string;
-  method: string
+  method: string;
+};
+
+export type Product = {
+  id: number;
+  timestamp: number;
+  name: string;
+  description: string;
+  code: number;
+  image: string;
+  price: number;
+  stock: number;
+};
+
+export type Cart = {
+  id: number;
+  products: Product[];
+  timestamp: number;
 };
