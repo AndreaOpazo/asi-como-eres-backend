776d95f5fbbd6f6162afc5a24574e1afc94b6212
AndreaOpazo <andree.2593@gmail.com>
2021-09-15 03:17:51 -0300

    reestructuro app

--- src/app.ts
+++ src/app.ts
@@ -1,22 +1,27 @@
 import express from 'express';
 import { Route } from './interfaces/routes.interface';
+import error404Middleware from './middlewares/error404.middleware';
 import errorMiddleware from './middlewares/error.middleware';
 
 class App {
   private app: express.Application;
   private port: (string | number);
-  // private env: boolean;
 
   constructor(routes: Route[]) {
     this.app = express();
     this.port = process.env.PORT || 8080;
-    // this.env = process.env.NODE_ENV === 'production' ? true : false;
+    this.initializeMiddlewares();
     this.initializeRoutes(routes);
     this.initializeErrorHandling();
   }
 
   public listen() {
     this.app.listen(this.port, () => console.log(`App listening on the port ${this.port}`));
+  }
+
+  private initializeMiddlewares() {
+    this.app.use(express.json());
+    this.app.use(express.urlencoded({ extended: true }));
   }
 
   private initializeRoutes(routes: Route[]) {
@@ -27,6 +32,7 @@ class App {
 
   private initializeErrorHandling() {
     this.app.use(errorMiddleware);
+    this.app.use(error404Middleware);
   }
 }
 
