97cd5c5fbe2a1afae135974a0284a691998d9cc2
AndreaOpazo <andree.2593@gmail.com>
2021-09-15 03:18:48 -0300

    correccion de ruta mal llamada

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,10 @@
-// import 'dotenv/config';
 import App from './app';
 import { ProductsRoute } from './routes/products.route';
-import { ChartRoute } from './routes/chart.route';
+import { CartRoute } from './routes/cart.route';
 
 const app = new App([
   new ProductsRoute(),
-  new ChartRoute(),
+  new CartRoute(),
 ]);
 
 app.listen();
